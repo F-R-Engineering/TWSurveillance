a9ddbc85448ede62515d84c7577ef6d6f2ee4d94
F-R-Engineering <107813443+F-R-Engineering@users.noreply.github.com>
2022-10-10 22:53:49 +0100

    testScript1.js

---
+++ testScript1.js
@@ -0,0 +1,16 @@
+function getHTML(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            callback(xhr.responseText);
+        }
+    }
+    xhr.send();
+};
+
+function callback(data) {
+    console.log(data);
+};
+
+getHTML('https://zz1.tribalwars.works/game.php?village=3106&screen=report', callback);
