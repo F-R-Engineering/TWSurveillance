c925538d107427b2b88748438b56b6017b974315
F-R-Engineering <107813443+F-R-Engineering@users.noreply.github.com>
2022-10-10 23:03:03 +0100

    Update testScript1.js

--- testScript1.js
+++ testScript1.js
@@ -1,12 +1,1 @@
-function getHTML(url) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-            return xhr.responseText;
-        }
-    }
-    xhr.send();
-};
-
-console.log(getHTML('https://zz1.tribalwars.works/game.php?village=3106&screen=report'));
+console.log("hi")
