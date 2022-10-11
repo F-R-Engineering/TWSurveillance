f8bc5afe1d032697b3a3c5526cf3b63b51e803d2
F-R-Engineering <107813443+F-R-Engineering@users.noreply.github.com>
2022-10-11 08:38:53 +0100

    Update testScript1.js

--- testScript1.js
+++ testScript1.js
@@ -1,1 +1,20 @@
-console.log("hi")
+function getLinks(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            callback(xhr.responseText);
+        }
+    }
+    xhr.send();
+}
+
+function callback(data) {
+    var links = data.match(/<a href="(.*?)"/g);
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].indexOf('screen=report&mode=') != -1) {
+            console.log(links[i]);
+        }
+    }
+}
+getLinks('https://zz1.tribalwars.works/game.php?village=3106&screen=report', callback);
